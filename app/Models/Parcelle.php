0067e7e63a4411217653ee4f647f6e184bf89390
walidkarouach <karouachwalid700@gmail.com>
2026-07-06 15:09:57 +0100

    Creation de Model, Migration, Factory, Seeder et Controller

---
+++ app/Models/Parcelle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Parcelle extends Model
+{
+    /** @use HasFactory<\Database\Factories\ParcelleFactory> */
+    use HasFactory;
+}
---
+++ database/factories/ParcelleFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Parcelle;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Parcelle>
+ */
+class ParcelleFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
