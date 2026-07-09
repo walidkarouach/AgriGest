12b13b74117d23bccbf5ed628bbf4f7f6c859352
walidkarouach <karouachwalid700@gmail.com>
2026-07-09 10:16:37 +0100

    Ajouter route de parcelles

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ParcelleController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('parcelles', ParcelleController::class);
