fe9db451a22bb38c6018b3fd4469471d3b94e7f8
walidkarouach <karouachwalid700@gmail.com>
2026-07-09 10:10:25 +0100

    Ajouter Seeders

--- database/seeders/ParcelleSeeder.php
+++ database/seeders/ParcelleSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Parcelle;
 
 class ParcelleSeeder extends Seeder
 {
@@ -12,6 +12,6 @@ class ParcelleSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Parcelle::factory(20)->create();
     }
 }
